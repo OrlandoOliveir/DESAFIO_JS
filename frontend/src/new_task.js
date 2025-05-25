0b307e3b4c3e62153a2b8eb799e4eef67b879a41
Orlando <orlandojunioroliveira@gmail.com>
2025-05-25 19:25:36 -0300

    Ambiente funcionando!

--- frontend/src/new_task.js
+++ frontend/src/new_task.js
@@ -1,7 +1,5 @@
-import {connectDB, mongoose} from 'backend/config/db.js';
 document.getElementById('save-btn').addEventListener('click', function(event) {
 
-  console.log('sdsds');
-    event.preventDefault();
 
+  
 });
