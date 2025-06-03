d3bbc2f3313f279b738d1a16ae9ff76825f3a4d9
Orlando <orlandojunioroliveira@gmail.com>
2025-06-02 23:13:54 -0300

    rota select funcionando

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,13 +9,22 @@ const taskSchema = new mongoose.Schema({
   data_agendado: Date
 });
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model('tarefas', taskSchema);
 
 router.post('/tasks', async (req, res) => {
   try {
     const { nome, status, data_agendado } = req.body;
     const newTask = await Task.create({ nome, status, data_agendado });
     res.status(201).json(newTask);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,22 @@
-import '../public/styles.css';
-import '../public/new_task.css';
+document.getElementById('refresh-btn').addEventListener('click', async(event) =>{
+try {
+    const response = await fetch('http://localhost:3000/api/tasks', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+        const tasks = await response.json();
+        console.log('Tarefas carregadas com sucesso!');
+        console.log(tasks);
+    } else {
+      throw new Error('Falha ao carregar tarefas');
+    }
+  } catch (error) {
+    console.error('Erro:', error);
+    alert('Erro ao carregar tarefas: ' + error.message);
+  }
+});
+
+
